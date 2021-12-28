e04dda0d5e3b99a5acabe9d8832be80aa974cc41
Артем <kysapka@gmail.com>
2021-12-28 19:26:21 +0300

    add new env example

---
+++ .env.example.ts
@@ -0,0 +1,8 @@
+# Base URL must be here
+REACT_APP_BASE_URL=
+#Your API KEY must be here
+REACT_APP_API_KEY=
+#Your e-mail adress must be here
+REACT_APP_EMAIL=
+#Your password must be here
+REACT_APP_PASSWORD=
